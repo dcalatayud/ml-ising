e7cdcb914018b3ef4b4be45748b8fbbe06e438db
David Calatayud <davidgcalatayud@gmail.com>
2023-12-12 00:22:00 +0100

    init

---
+++ MonteCarloSampling.c
@@ -0,0 +1,244 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define N_FLIPS_PER_STEP 1
+#define HOT_INIT 0
+
+/*
+ * Generates the initial configuration.
+ */
+void init(int *lattice, const int L)
+{
+    double r;
+    r = 1.0*rand() / RAND_MAX;
+    for(int row=0; row<L; row++) {
+        for(int col=0; col<L; col++) {
+            #if HOT_INIT == 1
+                // HOT init, all sites are random
+                r = 1.0*rand() / RAND_MAX; // Returns 0 or 1
+                lattice[row*L + col] = (r>0.5 ? 1 : -1);
+            #else
+                // COLD init, the same state for all the spins
+                lattice[row*L + col] = (r>0.5 ? 1 : -1);
+            # endif
+        }
+    }
+}
+
+
+/*
+ * Copies the lattice into an other lattice
+ */
+void copy(int *dest_lattice, const int *src_lattice, const int L) {
+    for(int row=0; row<L; row++) {
+        for(int col=0; col<L; col++) {
+            dest_lattice[row*L + col] = src_lattice[row*L + col];
+        }
+    }
+}
+
+
+/*
+ * Computes the energy in units of J
+ */
+int computeEnergy(const int *lattice, int L)
+{
+    int E = 0;
+    int idx,   // index of the current lattice site,
+        idx_r, // of its nearest neighbor on the right
+        idx_b; // and the one below
+    for(int row=0; row<L; row++) {
+        for(int col=0; col<L; col++) {
+            idx = row*L + col;
+            // Periodic Boundary Conditions
+            idx_r = (col == L-1) ? row*L : row*L + col+1;
+            idx_b = (row == L-1) ? col   : (row+1)*L + col;
+
+            E += -1*lattice[idx]*lattice[idx_r];
+            E += -1*lattice[idx]*lattice[idx_b];
+        }
+    }
+
+    return E;
+}
+
+
+/*
+ * Flips the designated number of flips.
+ * Beware that this function does not exclude
+ * multiple flips of the same bit
+ */
+int propose_configuration(int *lattice, int n_flips, int L, int E) {
+    int row, col,
+        idx_l, idx_a, idx_r, idx_b; //Left, above, right, below
+
+    for(int n=0; n<n_flips; n++) {
+        row = rand()%L;
+        col = rand()%L;
+
+        lattice[row*L+col] = -1*lattice[row*L+col];
+
+        idx_l = (col == 0)   ?   row*L+(L-1):     row*L + col-1;
+        idx_a = (row == 0)   ? (L-1)*L+col  : (row-1)*L + col;
+        idx_r = (col == L-1) ?   row*L      :     row*L + col+1;
+        idx_b = (row == L-1) ?         col  : (row+1)*L + col;
+
+        E +=  -2*lattice[row*L+col] * ( lattice[idx_l] +
+                                        lattice[idx_a] +
+                                        lattice[idx_r] +
+                                        lattice[idx_b]);
+    }
+    return E;
+}
+
+
+/*
+ * Perform a Monte Carlo step
+ * BEWARE that the input vector is changed!
+ */
+int montecarlo_step(int *lattice, int L, int E, double T) {
+    int E_prop; // Energy of the proposed configuration
+    int *lattice_prop = (int*) calloc(L*L, sizeof(int*));
+
+    copy(lattice_prop, lattice, L);
+    E_prop = propose_configuration(lattice_prop, N_FLIPS_PER_STEP, L, E);
+
+    // Acceptation step
+    double w = exp(- (E_prop-E)/T);
+    double r = 1.0*rand()/RAND_MAX;
+
+    if(r <= w) {        
+        copy(lattice, lattice_prop, L);
+        free(lattice_prop);
+        return 1; // Accepted
+    } else {
+        free(lattice_prop);
+        return 0; // Rejected
+    }
+
+}
+
+
+/*
+ * Prints the lattice.
+ *      spin=1 --> +
+ *      spin=0 --> O
+ */
+void printLattice(const int *lattice, const int L) {
+    for(int row=0; row<L; row++) {
+        for(int col=0; col<L; col++) {
+            if( lattice[row*L+col] == 1) {
+                printf("+");
+            } else {
+                printf("O");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+/*
+ * Save Ising lattice to file in CSV format
+ * The columns are, in order:
+ *      Lattice size, Temperature, Lattice Energy, lattice sites
+ */
+void save(FILE *fptr, int L, double T, int E, int *lattice) {
+    fprintf(fptr,"%d, %lf, %d", L, T, E);
+    for(int row=0; row<L; row++) {
+        for(int col=0; col<L; col++) {
+            fprintf(fptr, ", %d", lattice[row*L+col]);
+        }
+    }
+    fprintf(fptr, "\n");
+}
+
+
+/*
+ * Alternative to srand(time(NULL)) which keeps returning the same value
+ * due to very close callings
+ */
+unsigned long long rdtsc() {
+    unsigned int lo,hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((unsigned long long)hi << 32) | lo;
+}
+
+
+/*
+ * This program aims to generate Ising configurations
+ * on a lattice
+ */
+int main(int argc, char *argv[]) {
+    int L, E, NUMBER_OF_CONFIGURATIONS, THERMALISATION_STEPS, INITIAL_THERMALISATION_STEPS;
+    double T;
+    FILE *fptr;
+
+    THERMALISATION_STEPS = 100;
+    INITIAL_THERMALISATION_STEPS = 100;
+
+    // Parse command line arguments
+    for (int i = 0; i < argc; i++) {
+        if( strcmp(argv[i], "--lattice-side")==0  &&  i+1<argc ) {
+            L = atoi(argv[i+1]); // Side of the lattice
+        } else if( strcmp(argv[i], "--number-of-configurations")==0 &&  i+1<argc ) {
+            NUMBER_OF_CONFIGURATIONS = atof(argv[i+1]); // Steps
+        } else if( strcmp(argv[i], "--thermalisation-steps")==0 &&  i+1<argc ) {
+            THERMALISATION_STEPS = atof(argv[i+1]); // Steps
+        } else if( strcmp(argv[i], "--temperature")==0 &&  i+1<argc ) {
+            T = atof(argv[i+1]); // Temperature
+        } else if( strcmp(argv[i], "--initial-thermalisation-steps")==0 &&  i+1<argc ) {
+            INITIAL_THERMALISATION_STEPS = atof(argv[i+1]); // Steps
+        } else if( strcmp(argv[i], "--output-file")==0 &&  i+1<argc ) {
+            fptr = fopen(argv[i+1], "a+");
+            if(fptr == NULL) {
+                printf("Can not open file %s\n", argv[i+1]);
+                exit(1);
+            }
+        }
+    }
+
+    printf("################### SIMULATION INFO ###################\n");
+    printf("LATTICE SIDE: %d\n", L);
+    printf("TEMPERATURE: %f\n", T);
+    printf("NUMBER OF CONFIGURATIONS: %d\n", NUMBER_OF_CONFIGURATIONS);
+    printf("THERMALISATION_STEPS: %d\n", THERMALISATION_STEPS);
+    printf("INITIAL_THERMALISATION_STEPS: %d\n", THERMALISATION_STEPS);
+    printf("#######################################################\n\n");
+
+    int *lattice = (int*) calloc(L*L, sizeof(int));
+
+    srand(rdtsc());
+
+    init(lattice, L);
+
+    int acc, steps;
+
+    E = computeEnergy(lattice, L);
+    for(steps=0; steps<INITIAL_THERMALISATION_STEPS; steps++) {
+        acc+=montecarlo_step(lattice, L, E, T);
+    }
+
+    for(int i = 0; i<NUMBER_OF_CONFIGURATIONS; i++) {
+        E = computeEnergy(lattice, L);
+        acc = 0;
+        steps = 0;
+
+        while(steps<THERMALISATION_STEPS) {
+            steps++;
+            acc+=montecarlo_step(lattice, L, E, T);
+        }
+
+        montecarlo_step(lattice, L, E, T);
+        E = computeEnergy(lattice, L);
+        save(fptr, L, T, E, lattice);
+
+        printf("montecarlo_step: acceptation_rate=%d/%d=%f\t Energy=%d\t Temperature=%f\n", acc, steps, (double) acc/steps, E, T);
+    }
+
+    fclose(fptr);
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,93 @@
+# Machine Learning phases of matter
+## Abstract
+This project aims to reobtain the results presented in [Machine learning phases of matter (Juan Carrasquilla, Roger G. Melko)](https://arxiv.org/abs/1605.01735) regarding the two-dimensional square Ising model. The authors show that a standard _feed-forward neural network_ (FFNN) is a suitable tool to detect Ising's model phase transition.  
+This is done by first comparing the behavior of a _fully connected neural networr_ (FCNN) to a hand-made Toy model.  
+Then the number of parameters of the FCNN is increased and the accuracy of the model is studied as a function of the lattice size to remove boundary effects.   In the original article, the FCNN is then upgraded to a _convolutional neural network_ to test its accuracy on non-Ising Hamiltonians.  
+Finally, as an appendix, the original data is analyzed with the _t-stochastic neighbors embedding_ algorithm to separate the data into clusters.
+
+> :grey_exclamation: The project is subdivided into notebook written to be run on [google colab](https://colab.research.google.com/). A copy is provided in this repository but they may not run in your local environment. It is possible to access the original notebooks using the link provided. 
+
+## Notebooks
+The following sections present a summary of the results obtained. A detailed discussion is available inside the notebooks themselves. 
+
+## 01 Monte Carlo simulation
+The first notebook is centered around the Monte Carlo code `MonteCarloSampling.c` written to generate the configurations.
+In particular, in the notebook the behavior of the magnetization and the autocorrelation will be analyzed. Both tests will lead to positive results with a correlation length of $\simeq1000$ time steps.
+
+### Magnetization
+
+![](img/01_magnetization.png)
+
+### Autocorrelation
+![](img/01_autocorrelation.png)
+
+</br>
+
+## 02 The Toy model
+[Google colab link](https://colab.research.google.com/drive/1VhzjsGJBBggnKgkuA9-o77eyjR3w4s2m?usp=sharing)
+
+The second notebook focuses on the Toy Model presented in the article and examines the corresponding FCNN. The analytical toy model correctly classifies the temperature up to a parameter $\epsilon$ that was found by trial and error (an optimization procedure would have required more time without providing any real value). Then a FCNN with the same structure as the toy model is designed:
+- Input layer consisting of $L^2$ neurons;
+- Hidden layer with 3 neurons and a **sigmoid** activation function;
+- Output layer with 2 neurons and a **sigmoid** activation function.
+The activation function was switched from Heaviside to Sigmoid as suggested in the article to avoid the pathological behavior of the Heaviside's gradient.  
+The network's performances have a high dependence on the stochastic component of the training. If the training goes well the test loss is smaller than 0.1 percent, otherwise, it oscillates between 0.2 and 0.4 (this problem will be solved in the third notebook). When the training goes well both the hidden layer and the output layer provide similar results to the toy model and the critical temperature is correctly identified within a small error.
+
+#### Output of the analytical Toy model
+![](img/02_Analytical_Toy_model.png)
+
+#### Training of the FCNN Toy Model
+![](img/02_FCNN_training_loss.png)
+
+#### Output of the FCNN Toy model
+![](img/02_FCNN_Toy_model_magnetization.png)
+![](img/02_FCNN_Toy_model_temperature.png)
+
+</br>
+
+## 03 The Network in detail
+The third notebook will be more theory-oriented. First, the Toy model FCNN will be upgraded with a hidden layer of 100 neurons, then the training procedure will be discussed in detail. Following that, the results of [Juan Carrasquilla and Roger G. Melko](https://arxiv.org/abs/1605.01735) on non-standard Ising hamiltonian will be presented.
+
+### Training of the improved network
+<a href="https://ibb.co/TmwDDKW"><img src="https://i.ibb.co/xJ2KKmF/03-test.png" alt="03-test" border="0"></a>
+
+</br>
+
+## 04 t-SNE
+[Google colab link](https://colab.research.google.com/drive/1hUy1Fy7iUTkWHX-S79e-WppUdJBHMw9E?usp=sharing)
+
+The fourth notebook will try to implement the content of _Appendix B: Visualizing the action of a neural network on the Ising ferromagnet_ using scikit-learn's implementation of t-SNE.
+
+**Results for various values of perplexity**  
+As t-SNE uses a non-convex cost function the plot will always look different. Some plots obtained are reported _for clarity_. As they were obtained by repetitive runs until a "good-looking" plot was produced, they have little scientific meaning.
+
+- Perplexity 100; Early Exaggeration 195
+
+<a href="https://ibb.co/9tgwpmV"><img src="https://i.ibb.co/ySPhNmY/final-perp100-exag195.png" alt="final-perp100-exag195" border="0"></a>
+
+- Perplexity 100; Early Exaggeration 50; points around $T_C$ used
+
+<a href="https://ibb.co/0BxK71p"><img src="https://i.ibb.co/1skMgBY/04-perplexity100.png" alt="04-perplexity100" border="0"></a>
+
+- Perplexity 50; Early Exaggeration 50; points around $T_C$ used
+
+<a href="https://ibb.co/KwTwyVm"><img src="https://i.ibb.co/X8c8JFV/04-perplexity50.png" alt="04-perplexity50" border="0"></a>
+
+- Perplexity 30; Early Exaggeration 50; points around $T_C$ used
+
+<a href="https://ibb.co/pKwh68F"><img src="https://i.ibb.co/092spSb/04-perplexity30.png" alt="04-perplexity30" border="0"></a>
+
+- Perplexity 10; Early Exaggeration 50; points around $T_C$ used
+
+<a href="https://ibb.co/k2LvVyY"><img src="https://i.ibb.co/DfBTPb3/04-perplexity10.png" alt="04-perplexity10" border="0"></a>
+
+- Perplexity 4; Early Exaggeration 50; points around $T_C$ used
+
+<a href="https://ibb.co/Hqd1HP0"><img src="https://i.ibb.co/L15bZgM/04-perplexity4.png" alt="04-perplexity4" border="0"></a>
+
+</br>
+
+---
+
+</br>
+
